4a25245712bb103c05a7344f4a25996eb8b6edfd
Estevez <estevezb@wit.edu>
2022-04-04 13:14:40 -0400

    Added Random word selection

--- src/Main/bareBones.java
+++ src/Main/bareBones.java
@@ -1,5 +1,6 @@
 package Main;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class bareBones {
@@ -8,7 +9,8 @@ public class bareBones {
 		
 		String[] words = {"Main","Moths", "Loath", "Razer", "Youth", "Three"};
 		
-		String word = "hello";
+		
+		String word = getWord(words);
 		Scanner input = new Scanner(System.in);
 		
 		int g = 6;
@@ -50,6 +52,11 @@ public class bareBones {
 			System.out.println("\n You failed, the word was " + word);
 		}
 		
+	}
+	
+	public static String getWord(String[] array) {
+		int rnd = new Random().nextInt(array.length);
+		return array[rnd];
 	}
 	
 }
